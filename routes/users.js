0260d3a3febce3e6573bdb20a19140c8b67d8195
rashmihp <rashmihp99@gmail.com>
2018-09-19 00:31:30 -0700

    typo fixed

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.post('/auth', master.auth)
 router.get('/profile', master.profile)
 // temporary route /temp
 router.get('/click/:x/:y', master.temp)
-router.get('/ask/:x', master.temp)
+router.get('/ask/:x', master.ask)
 
 /*router.get('/rashmi', middlewear1, middlewear2, finalfunction)
 

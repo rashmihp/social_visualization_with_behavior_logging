7926f775089fba7b12a59aff20b2c6d3c1501255
rashmihp <rashmihp99@gmail.com>
2018-09-18 20:00:35 -0700

    blah blah

--- app.js
+++ app.js
@@ -9,12 +9,24 @@ const routes = require('./routes/users');
 app.set('views', path.join(__dirname, 'views')); // path to the views folder
 app.set('view engine', 'pug');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.get('/temp/:num', function(req, res, next) {
+  console.log(req.params) // parse req.body and put to databse as required...
+  res.send("workeds")
+});
+app.post('/', function(req, res, next) {
+ console.log("abc")
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/',routes)
 app.use(express.static('css')); //load files from public directory using express.
 app.use(session({secret: 'ssshhhhh'}));
 
-app.listen(process.env.PORT, () => {
+app.listen(3000, () => {
   //console.log(`App is running on: ${port}`)
   mongo.connectToDatabase().then(e => console.log("Connected to database..."))
 });
--- extensions/content.js
+++ extensions/content.js
@@ -6,17 +6,20 @@ let paragraphs = document.getElementsByClassName('d-inline-flex ai-center ws-now
 for(elt of paragraphs){
   elt.style['background-color'] = '#FF00F0'
 }
-
 //for random clicks on the screen
 var classname = document.getElementsByClassName("tagged-questions-page unified-theme new-topbar")
 classname[0].addEventListener("click", getClickPosition, false);
 function getClickPosition(e) {
-    //var xPosition = e.clientX;
-    //var yPosition = e.clientY;
-    //console.log("x =" +xPosition + ", y= "+ yPosition + Date())
+    var d = new Date()
+    var xPosition = e.clientX;
+    var yPosition = e.clientY;
 
+    console.log("x =" +xPosition + ", y= "+ yPosition + d.getHours()+":"+d.getMinutes()+":"+d.getSeconds())
+    var myObj = { x: xPosition, y: yPosition, date: d.getHours()+":"+d.getMinutes()+":"+d.getSeconds() };
+    var myJSON = JSON.stringify(myObj);
+    console.log(myObj);
   var xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "http://localhost:3000/temp?key=value", true);
+  xhttp.open("GET", "http://localhost:3000/temp/123", true);
   xhttp.send(); // 2min......ur site (localhost) is not running in https...thats y error. yreah thats what i was tellling
   //https://stackoverflow.com/questions/11744975/enabling-https-on-express-js
   //use this link to setup https then it will work... yeahh okk ill try
@@ -28,11 +31,23 @@ function getClickPosition(e) {
 var classname = document.getElementsByClassName("d-inline-flex ai-center ws-nowrap s-btn s-btn__primary")
 classname[0].addEventListener("click", askQuestion);
 function askQuestion() {
-    console.log("Question asked" + Date())
+    var d = new Date()
+    console.log("Question asked" + d.getHours()+":"+d.getMinutes()+":"+d.getSeconds())
   }
 var classname = document.getElementsByClassName("tagged-questions-page unified-theme new-topbar")
-classname[0].addEventListener("wheel", myFunction);
+classname[0].addEventListener("wheel", myFunction, false);
 
-  function myFunction() {
-      console.log("scrolling")
+  function myFunction(e) {
+      var d = new Date()
+      var xPosition = e.clientX;
+      var yPosition = e.clientY;
+      console.log("scrolling " + " x = " + xPosition + " y = " + yPosition + "time:" + d.getHours()+":"+d.getMinutes()+":"+d.getSeconds())
   }
+
+
+  $(document).ready(function() {
+    console.log('jquery loaded');
+    $.get("http://localhost:3000/temp/data", function(res) {
+      console.log(res)
+    })
+  })
